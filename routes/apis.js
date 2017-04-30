ccb765af9b3993da82831aa9fc3739aca070fdda
Viosey <imviosey@gmail.com>
2017-04-30 23:34:19 +0800

    update
    - add router /

--- routes/apis.js
+++ routes/apis.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
+// var fs = require('fs');
 
-/* GET material API. */
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  var sites = [
+    ["Material Theme", "https://api.viosey.com/material", "GET"],
+    ["Paranoia", ""]
+  ];
+  res.render('index', { 
+    title: "Viosey's API",
+    sites: sites
+  });
+});
+
+/* GET Material API. */
 router.get('/material', function(req, res) {
   res.json({ version: "1.3.4" });
 });
+
+/* GET Material API. */
+// router.get('/paranoia', function(req, res) {
+//   fs.readFile('https://viosey.com/paranoia.json', function(err, data){
+//     if(err) console.log(err);
+
+//     var jsonObj = JSON.parse(data);
+//     var word = jsonObj[0];
+//     console.log(word);
+//   });
+//   res.send('word');
+// });
 
 module.exports = router;
